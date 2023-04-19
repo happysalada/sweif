67f593485aacea0c2f90adbe126a4a4e7e7f6910
happysalada <raphael@megzari.com>
2023-04-19 16:56:53 -0400

    deposit page styles

--- src/routes/[user]/deposit/+page.svelte
+++ src/routes/[user]/deposit/+page.svelte
@@ -55,14 +55,14 @@
   								id="bankAccount"
   								name="bankAccount"
   								autocomplete="bankAccount"
-  								class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+  								class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:max-w-xs sm:text-sm sm:leading-6"
   							>
   								<option value="CO">Bancolombia</option>
   							</select>
   						</div>
   					</div>
 
-            <button type="button" class="rounded-md bg-indigo-600 px-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">New account</button>
+            <button type="button" class="rounded-md bg-primary-600 px-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600">New account</button>
           </div>
         </dd>
       </div>
@@ -73,10 +73,10 @@
           <div class="flex justify-start gap-8">
             <div>
               <label for="email" class="sr-only">Amount</label>
-              <input type="number" name="amount" id="amount" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="0.00">
+              <input type="number" name="amount" id="amount" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6" placeholder="0.00">
             </div>
 
-            <button type="button" class="rounded-md bg-indigo-600 px-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Submit</button>
+            <button type="button" class="rounded-md bg-primary-600 px-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600">Submit</button>
           </div>
         </dd>
       </div>
