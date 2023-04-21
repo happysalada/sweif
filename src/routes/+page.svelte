b48e7fb50db74e6aeda3c1c7e07e6bef18681c89
happysalada <raphael@megzari.com>
2023-04-21 10:50:23 -0400

    link fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -57,11 +57,11 @@
         </p>
         <div class="mt-10 flex items-center gap-x-6">
           <a
-            href="#"
+            href="/signin"
             class="rounded-md bg-primary-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
             >Crea tu cuenta</a
           >
-          <a href="#" class="text-sm font-semibold leading-6 text-gray-900"
+          <a href="#features" class="text-sm font-semibold leading-6 text-gray-900"
             >Más información<span aria-hidden="true">→</span></a
           >
         </div>
