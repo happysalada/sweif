ccb6e84dccec1fb0d931244cbb47c3278593dae8
happysalada <raphael@megzari.com>
2023-05-02 11:36:59 -0400

    remove serpapi ref until polyfills have been sorted

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,17 +1,19 @@
 import type { PageServerLoad } from './$types';
-import { getJson } from "serpapi";
+// import { getJson } from "serpapi";
 
-export const load = (async ({ platform }) => {
+export const load = (async ({ platform, fetch }) => {
   let api_key = platform?.env?.SERPAPI_KEY;
   if (!api_key) {
     return {
       fx: "missing_api_key"
     }
   }
-  let eurCop = await getJson("google_finance", {
-        api_key, // Get your API_KEY from https://serpapi.com/manage-api-key
-        q: "EUR-COP",
-      })
+  // let eurCop = await getJson("google_finance", {
+  //       api_key, // Get your API_KEY from https://serpapi.com/manage-api-key
+  //       q: "EUR-COP",
+  //     })
+  let eurCopResponse = await fetch(`https://serpapi.com/search?engine=google_finance&q=${"EUR-COP"}&api_key=${api_key}`)
+  let eurCop = await eurCopResponse.json();
   let eurCopRate: number = eurCop.summary?.extracted_price;
   let copEurRate = eurCopRate ? 1 / eurCopRate: 1.0 ;
   return {
