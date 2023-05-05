6d46b172c0ccbf6e51fc94158da691a3548834f0
happysalada <raphael@megzari.com>
2023-05-05 16:37:11 -0400

    text fix

--- src/lib/DepositConfirmModal.svelte
+++ src/lib/DepositConfirmModal.svelte
@@ -123,12 +123,16 @@
               <dl class="divide-y divide-gray-100">
                 {#if state == State.Transfer}
                 <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                  <dt class="text-sm font-medium text-gray-900">Transfiere</dt>
+                  <dt class="text-sm font-medium text-gray-900">Monto a Transferir</dt>
                   <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
                     {inputAmount} {inputCurrency}
                   </dd>
                 </div>
                 <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                  <dt class="text-sm font-medium text-gray-900 text-right">Nombre de la sociedad</dt>
+                  <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                    Bitval Colombia SAS
+                  </dd>
                   <dt class="text-sm font-medium text-gray-900 text-right">NIT</dt>
                   <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
                     901617634-7
