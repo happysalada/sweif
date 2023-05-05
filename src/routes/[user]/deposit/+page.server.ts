d09a0563f1af1d0f088878e5e6559bca92bf8567
happysalada <raphael@megzari.com>
2023-05-05 11:30:19 -0400

    add missing fxrates to withdraw

--- src/routes/[user]/deposit/+page.server.ts
+++ src/routes/[user]/deposit/+page.server.ts
@@ -6,12 +6,24 @@ export const load = (async ({ platform, fetch }) => {
     return {
     }
   }
-  let rateResponse = await fetch(`https://serpapi.com/search?engine=google_finance&q=${"EUR-COP"}&api_key=${api_key}`)
-  let rate = await rateResponse.json();
-  let rateNumber: number = rate.summary?.extracted_price;
+
+  let eurCopRateResponse = await fetch(`https://serpapi.com/search?engine=google_finance&q=${"EUR-COP"}&api_key=${api_key}`)
+  let eurCopRate = await eurCopRateResponse.json();
+  let eurCopRateNumber: number = eurCopRate.summary?.extracted_price;
+  let eurUsdRateResponse = await fetch(`https://serpapi.com/search?engine=google_finance&q=${"EUR-COP"}&api_key=${api_key}`)
+  let eurUsdRate = await eurUsdRateResponse.json();
+  let eurUsdRateNumber: number = eurUsdRate.summary?.extracted_price;
+  let usdCopRateResponse = await fetch(`https://serpapi.com/search?engine=google_finance&q=${"EUR-COP"}&api_key=${api_key}`)
+  let usdCopRate = await usdCopRateResponse.json();
+  let usdCopRateNumber: number = usdCopRate.summary?.extracted_price;
   return {
     fxRates: {
-      "EURC-COP": rateNumber,
+      "EURC-EUR": 1.0,
+      "EUR-USDC": eurUsdRateNumber,
+      "EUR-USDT": eurUsdRateNumber,
+      "EURC-COP": eurCopRateNumber,
+      "USDC-COP": usdCopRateNumber,
+      "USDT-COP": usdCopRateNumber,
     },
   };
 }) satisfies PageServerLoad;
--- src/routes/[user]/withdraw/+page.server.ts
+++ src/routes/[user]/withdraw/+page.server.ts
@@ -5,13 +5,41 @@ import { EUR_FEE_TABLE } from "$lib/constants";
 // import { getJson } from "serpapi";
 
 export const load = (async ({ platform, fetch }) => {
+  let api_key = platform?.env?.SERPAPI_KEY;
+  if (!api_key) {
+    return {
+      fxRates: {
+        "EURC-EUR": 1.0,
+      },
+      feeTable: {
+        [FiatCurrency.EUR]: decimalToStringTable(EUR_FEE_TABLE),
+        [StableCoin.EURC]: decimalToStringTable(EUR_FEE_TABLE),
+      }
+    }
+  }
+  let eurCopRateResponse = await fetch(`https://serpapi.com/search?engine=google_finance&q=${"EUR-COP"}&api_key=${api_key}`)
+  let eurCopRate = await eurCopRateResponse.json();
+  let eurCopRateNumber: number = eurCopRate.summary?.extracted_price;
+  let eurUsdRateResponse = await fetch(`https://serpapi.com/search?engine=google_finance&q=${"EUR-COP"}&api_key=${api_key}`)
+  let eurUsdRate = await eurUsdRateResponse.json();
+  let eurUsdRateNumber: number = eurUsdRate.summary?.extracted_price;
+  let usdCopRateResponse = await fetch(`https://serpapi.com/search?engine=google_finance&q=${"EUR-COP"}&api_key=${api_key}`)
+  let usdCopRate = await usdCopRateResponse.json();
+  let usdCopRateNumber: number = usdCopRate.summary?.extracted_price;
   return {
     fxRates: {
       "EURC-EUR": 1.0,
+      "EUR-USDC": eurUsdRateNumber,
+      "EUR-USDT": eurUsdRateNumber,
+      "EURC-COP": eurCopRateNumber,
+      "USDC-COP": usdCopRateNumber,
+      "USDT-COP": usdCopRateNumber,
     },
     feeTable: {
       [FiatCurrency.EUR]: decimalToStringTable(EUR_FEE_TABLE),
       [StableCoin.EURC]: decimalToStringTable(EUR_FEE_TABLE),
+      [StableCoin.USDC]: decimalToStringTable(EUR_FEE_TABLE),
+      [StableCoin.USDT]: decimalToStringTable(EUR_FEE_TABLE),
     }
   };
 }) satisfies PageServerLoad;
