14b732c906f51421735a3423db2fd39a0ccdf116
happysalada <raphael@megzari.com>
2023-05-05 15:14:09 -0400

    use initial strings for balance values

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,14 +1,12 @@
-import { Decimal } from 'decimal.js';
 import { persisted } from 'svelte-local-storage-store'
-import { writable } from 'svelte/store';
 
 import { StableCoin, FiatCurrency } from '$lib/types';
 
 const initialBalance = {
-  [StableCoin.EURC]: new Decimal(0),
-  [FiatCurrency.COP]: new Decimal(0),
-  [StableCoin.USDC]: new Decimal(0),
-  [StableCoin.USDT]: new Decimal(0),
+  [StableCoin.EURC]: '0',
+  [FiatCurrency.COP]: '0',
+  [StableCoin.USDC]: '0',
+  [StableCoin.USDT]: '0',
 }
 
 export const user = persisted<User | null>('user', null);
