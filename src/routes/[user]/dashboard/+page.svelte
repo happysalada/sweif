173f1bdd238f5839109cfad611d26eccb2a53a2b
happysalada <raphael@megzari.com>
2023-05-05 17:04:26 -0400

    fix dashboard recargar link

--- src/routes/[user]/dashboard/+page.svelte
+++ src/routes/[user]/dashboard/+page.svelte
@@ -26,8 +26,8 @@
       </p>
     </div>
     <a
-      href={`${$user?.email || 'nico'}/deposit`}
-      class="rounded-md bg-primary-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600 mt-6 w-80"
+      href={`/${$user?.email || 'nico'}/deposit`}
+      class="rounded-md bg-primary-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600 mt-6 w-80 text-center"
       >Recargar</a
     >
   </div>
