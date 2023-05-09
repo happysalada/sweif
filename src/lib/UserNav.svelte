1e2b9b1f00dc97444ccd478eca7d91848a434b0d
happysalada <raphael@megzari.com>
2023-05-09 12:12:33 -0400

    add balances title

--- src/lib/UserNav.svelte
+++ src/lib/UserNav.svelte
@@ -15,6 +15,9 @@
       <div
         class="mt-1 flex flex-col sm:mt-0 sm:flex-row sm:flex-wrap sm:space-x-6"
       >
+        {#if Object.entries($balances).filter(([currency, stringAmount]) => !(new Decimal(stringAmount).isZero())).length !== 0}
+          <p class="text-white text-lg mt-2 font-medium">Balances:</p>
+        {/if}
         {#each Object.entries($balances) as [currency, stringAmount]}
           {@const amount = new Decimal(stringAmount)}
           {#if !amount.isZero()}
