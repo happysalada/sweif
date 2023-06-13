5feb6893be30acff6a3d83794097db2b1294d32b
happysalada <raphael@megzari.com>
2023-06-13 07:43:18 -0400

    remove more components

--- src/lib/kyc/BackButton.svelte
+++
@@ -1,5 +0,0 @@
-<button class="pointer border-none outline-none z-10" on:click>
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="text-white w-10 h-10 absolute top-8 left-8">
-    <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" stroke-width="2"/>
-  </svg>
-</button>
--- src/lib/kyc/stores.ts
+++
@@ -1,6 +0,0 @@
-import { writable } from 'svelte/store';
-import { Flow, UploadType, IdCardStep } from './types';
-
-export const flow = writable<Flow>(Flow.Questionaire);
-export const uploadType = writable<UploadType>(UploadType.ID_CARD)
-export const idCardStep = writable<IdCardStep>(IdCardStep.FRONT)
